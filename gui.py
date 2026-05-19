de523bdd427c4cf2a3cc3bd0cd33165b763dfa07
sharib54009 <mohammedsharib0704@gmail.com>
2026-05-19 14:00:36 +0530

    TODO GUI completed

--- gui.py
+++ gui.py
@@ -1,24 +1,31 @@
 import PySimpleGUI as sg
 import functions
+import time
 
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True,
                       size=[45, 10] )
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 
 
 window = sg.Window("My TO-DO App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[clock],
+                           [label], 
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button], 
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=1000)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -39,6 +46,18 @@ while True:
                 window['todo'].update(value='')
             except IndexError:
                 sg.popup("Please select a todo to edit.")
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select a todo to complete.") 
+        case "Exit":
+            break
         case "todos":
             window['todo'].update(value=values['todos'][0].strip())
         case sg.WIN_CLOSED:
